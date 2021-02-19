ff368158799c70798dee4e6328c27336b41be4cc
Pablo Lago Castro <plagocastro@danielcastelao.org>
2021-02-19 09:20:14 +0100

    commit1

--- src/clase1.java
+++
@@ -1,5 +0,0 @@
-public class clase1 {
-    public void comer(){
-        System.out.println("ta comiendo");
-    }
-}
---
+++ src/package1/clase1.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class clase1 {
+        public void comer(){
+            System.out.println("ta comiendo");
+        }
+    }
